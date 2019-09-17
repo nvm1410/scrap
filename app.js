0fd756aa6a45d0097bc68a11bc9c94705d46875b
nvm1410 <42210786+nvm1410@users.noreply.github.com>
2019-09-17 14:49:53 +0700

    add

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const puppeteer = require('puppeteer');
 const youtube = require('./youtube');
+const express= require("express");
+const app=express();
+app.get("/",function (req,res){
+    // try {
+    //     (async () => {
+    
+    //         let keywords = [
+    //             'bts', 
+    //         ];
+    
+    //         const browser = await puppeteer.launch();
+    //         let results = await youtube.scrape_youtube(browser, keywords);
+    //         res.render(results, {depth: null, colors: true});
+    
+    //         await browser.close();
+    
+    //     })()
+    // } catch (err) {
+    //     console.error(err)
+    // }
+    res.render("hello")
+})
 
-try {
-    (async () => {
 
-        let keywords = [
-            'scrapeulous.com',
-            'scraping youtube',
-            'stupid prank',
-        ];
-
-        const browser = await puppeteer.launch();
-        let results = await youtube.scrape_youtube(browser, keywords);
-        console.dir(results, {depth: null, colors: true});
-
-        await browser.close();
-
-    })()
-} catch (err) {
-    console.error(err)
-}
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("Server has started");
+})
